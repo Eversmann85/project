52b639095feea7a248e42318212912e76b338e49
Dennis Eversmann <dennis.eversmann@rwth-aachen.de>
2019-12-07 18:19:06 +0100

    Setup Django Infrastructure.

--- project/__init__.py
+++ project/__init__.py
@@ -1,2 +1,7 @@
-from setuptools_scm import get_version
-version = get_version(root='..', relative_to=__file__)
+__version__ = "0.1.0"
+__version_info__ = tuple(
+    [
+        int(num) if num.isdigit() else num
+        for num in __version__.replace("-", ".", 1).split(".")
+    ]
+)
